de50e0280a2a6f5f0906c827f10e6ce46fc1490e
Ahmet Cilingir <face4forev@icloud.com>
2026-09-08 10:11:04 +0200

    Setup tab bar navigation

---
+++ MovieBox/ViewControllers/FavoritesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FavoritesViewController.swift
+//  MovieBox
+//
+//  Created by Ahmet CILINGIR on 08.09.26.
+//
+
+import UIKit
+
+final class FavoritesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ MovieBox/ViewControllers/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  MovieBox
+//
+//  Created by Ahmet CILINGIR on 08.09.26.
+//
+
+import UIKit
+
+final class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
